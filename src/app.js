bac8adfbc72b4f8c44f0bae20ba298a8d63865a9
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-27 23:59:03 -0500

    feat: :sparkles: manejadores de rutas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import appPruebas from './routers/pruebasRouter.js' 
+import appShopkeeper from './routers/shopkeeperRouter.js';
+import appUser from './routers/userRouter.js';
+import appPayment from './routers/paymentRouter.js';
 
 
 dotenv.config()
@@ -10,6 +13,10 @@ const appExpress = express();
 appExpress.use(express.json());
 
 appExpress.use('/prueba', appPruebas);
+appExpress.use('/shopkeeper', appShopkeeper);
+appExpress.use('/user', appUser);
+appExpress.use('/payment', appPayment);
+
 
 let config = JSON.parse(process.env.MY_SERVER);
 appExpress.listen(config, () =>{
